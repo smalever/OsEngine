835d9963aabc77b9ba057984301c2fac8861267e
smalever <smalever@gmail.com>
2023-05-28 10:50:49 +0300

    test1

---
+++ project/OsEngine/Robots/MyBots/PairTrade.cs
@@ -0,0 +1,155 @@
+﻿/*
+ * вводим пару валют в одну строку типа TRBUSDT/AAVEUSDT
+ * вводим сумму на которую хотим открыть пару
+ * выбираем направление торговли Long Short
+ * вводим ожидаемую прибыль в процентах на парную сделку
+ * 
+ * исходя из текущих цен по каждой бумаги бот должен посчитать количество лотов на каждую бумагу
+ * и выставить соответственно если Long то Long по первой бумаге и Short  по второй
+ * и наоборот если выбран Short d настройках
+ * 
+ * после открытия позиции ведет суммарную позицию до ожидаемой прибыли от сделки в процентах
+ * после достижения ожидаемой прибыли начинает трейлить каждую ногу на размер трейлинг в процентах от начальной сделки
+ * 
+ *  после закрытия обоих ног удаляет себя и закрывает за собой вкладки
+ * 
+ */
+
+
+/*
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using OsEngine.Charts.CandleChart.Indicators;
+using OsEngine.Entity;
+using OsEngine.Market;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.OsTrader.Panels.Attributes;
+
+namespace OsEngine.Robots.MyBots.PairTrade
+{
+    [Bot("PairTrade")]
+    public class PairTrade : BotPanel
+    {
+
+        public PairTrade(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+
+
+
+            TabCreate(BotTabType.Index);
+            _tabSynthetics = TabsIndex[0];
+
+            TabCreate(BotTabType.Simple);
+            _tab1 = TabsSimple[0];
+            TabCreate(BotTabType.Simple);
+            _tab2 = TabsSimple[1];
+
+
+            Regime = CreateParameter("Regime", "On", new[] { "Off", "On", "ClosePosition" });
+
+
+
+            // open leg1 leg2
+
+        }
+
+
+
+
+
+        #region TradeLogic ==============================
+
+        /// <summary>
+        /// trade logic
+        /// торговая логика
+        /// </summary>
+        private void OpenPairLegs(Leg1, List<Candle> candlesTab2, List<Candle> candlesIndex)
+        {
+
+
+
+            // open leg1
+            // open leg2
+
+
+
+        }
+
+
+
+
+
+            #endregion TradeLogic ==============================
+
+
+
+
+
+            #region Methods ==============================
+
+
+            private void LogicOpenPosition1(BotTabSimple tab, decimal lastRsi)
+        {
+
+
+        }
+
+        private void LogicOpenPosition2(BotTabSimple tab, decimal lastRsi)
+        {
+
+
+        }
+
+        public override string GetNameStrategyType()
+        {
+            return "PairTrade";
+        }
+
+        public override void ShowIndividualSettingsDialog()
+        {
+            // nothing
+        }
+
+        #endregion Methods ==============================
+
+
+
+
+        #region Fields ==============================
+
+        /// <summary>
+        /// index tab
+        /// вкладка для формирования синтетики пары
+        /// </summary>
+        private BotTabIndex _tabSynthetics;
+
+        /// <summary>
+        /// trade tab
+        /// вкладка для торговли
+        /// </summary>
+        private BotTabSimple _tab1;
+
+        /// <summary>
+        /// trade tab
+        /// вкладка для торговли
+        /// </summary>
+        private BotTabSimple _tab2;
+
+        /// <summary>
+        /// regime
+        /// режим работы робота
+        /// </summary>
+        public StrategyParameterString Regime;
+
+
+
+
+
+
+        #endregion Fields ==============================
+
+    }
+}
+// */
---
+++ project/OsEngine/Robots/MyBots/PriceChanelFix.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using OsEngine.Charts.CandleChart.Elements;
+using OsEngine.Entity;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.OsTrader.Panels.Attributes;
+using OsEngine.Indicators;
+using System.Windows.Forms;
+using System.Linq;
+
+[Bot("PriceChanelFix")]
+
+
+public class PriceChanelFix : BotPanel
+{
+    public PriceChanelFix(string name, StartProgram startProgram) : base(name, startProgram)
+    {
+        #region  подготовка вкладки ===================================
+        // делаем вкладку
+        this.TabCreate(BotTabType.Simple);
+        _tab = TabsSimple[0];
+
+        Mode = this.CreateParameter("Mode", "Off", new[] { "Off", "On" });
+
+        // parameters for indicator PriceChannel
+        LengthChannelUp = CreateParameter("Length Channel Up", 12, 5, 80, 2);
+        LengthChannelDown = CreateParameter("Length Channel Down", 12, 5, 80, 2);
+        // add indicator PriceChannel
+        _pc = IndicatorsFactory.CreateIndicatorByName("PriceChannel",name + "PriceChannel", false);
+        _pc.ParametersDigit[0].Value = LengthChannelUp.ValueInt;
+        _pc.ParametersDigit[1].Value = LengthChannelDown.ValueInt;
+        _pc = (Aindicator)_tab.CreateCandleIndicator(_pc, "Prime"); // 
+        _pc.Save();
+ 
+
+        Lot = CreateParameter("Lot", 1, 5, 20, 1);
+
+        RiskInPercents = CreateParameter("RiskInPercents", 1m, 1m, 10m, 0.1m);
+        #endregion конец подготовка вкладки ===================================
+
+        _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
+
+
+    }
+
+
+
+
+
+
+
+    #region Trade Logic ==============================
+
+    // 
+    private void _tab_CandleFinishedEvent(List<Candle> candles)
+    {
+        if (Mode.ValueString == "Off") return;
+        
+        // защита чтоб индикатор уже набрался свечек
+        if (
+            _pc.DataSeries[0].Values == null
+            || _pc.DataSeries[1].Values == null
+            || _pc.DataSeries[0].Values.Count < LengthChannelUp.ValueInt + 1
+            || _pc.DataSeries[1].Values.Count < LengthChannelDown.ValueInt + 1
+            ) return;
+
+        Candle candle = candles[candles.Count - 1];
+
+        // предыдущее за последней свечкой
+        decimal lastUp = _pc.DataSeries[0].Values[_pc.DataSeries[0].Values.Count - 2];
+        decimal lastDown = _pc.DataSeries[1].Values[_pc.DataSeries[1].Values.Count - 2];
+
+        List<Position> positions = _tab.PositionsOpenAll;
+
+        if (candle.Close > lastUp
+            && candle.Open<lastUp
+            && positions.Count ==0)
+        {
+
+            // сколько дененег от депо на 1 лот 
+            decimal riskMoney = _tab.Portfolio.ValueBegin * RiskInPercents.ValueDecimal / 100;
+            // получим стоимость шага цены
+            decimal costPriceStep = _tab.Securiti.PriceStepCost;
+            
+
+            // закоментить на боевом
+            costPriceStep = 1;
+
+            // шагов цены на стоп 
+            decimal steps = (lastUp - lastDown) / _tab.Securiti.PriceStepCost ;
+
+            // 
+            decimal lot = riskMoney / (steps * costPriceStep);
+
+            // _tab.BuyAtMarket(Lot.ValueInt);
+            _tab.BuyAtMarket((int)lot);
+
+        }
+
+        if (positions.Count > 0)
+        {
+            Trailing(positions);
+        }
+
+
+
+    }
+    #endregion end Trade Logic ==============================
+
+
+
+
+    #region Methods ==============================
+
+
+    private void Trailing(List<Position> positions)
+    {
+        // последнее значение нижней линии индикатора прайсченел
+        decimal lastDown = _pc.DataSeries[1].Values[_pc.DataSeries[1].Values.Count - 1];
+
+        foreach (Position pos in positions)
+        {
+            if (pos.State == PositionStateType.Open)
+            {
+                if (pos.Direction == Side.Buy)
+                {
+                    _tab.CloseAtTrailingStop(pos, lastDown, lastDown - 10 * _tab.Securiti.PriceStep);
+                }
+            }
+
+        }
+    }
+
+
+    // вернуть имя
+    public override string GetNameStrategyType()
+    {
+        return nameof(PriceChanelFix);
+    }
+    // для настроек
+    public override void ShowIndividualSettingsDialog()
+    {
+
+    }
+
+    #endregion end Methods ==============================
+
+
+
+
+    #region Fields ==============================
+    /// <summary>
+    /// вкладка для торговли
+    /// </summary>
+    private BotTabSimple _tab;
+
+    /// <summary>
+    /// PriceChannel
+    /// </summary>
+    private Aindicator _pc;
+
+    /// <summary>
+    /// PriceChannel up line length
+    /// период PriceChannel Up
+    /// </summary>
+    private StrategyParameterInt LengthChannelUp;
+
+    /// <summary>
+    /// PriceChannel down line length
+    /// период PriceChannel Down
+    /// </summary>
+    private StrategyParameterInt LengthChannelDown;
+
+    /// <summary>
+    /// режим торгуем - редактируем
+    /// </summary>
+    private StrategyParameterString Mode;
+
+    /// <summary>
+    /// кол-во лотов на сделку
+    /// </summary>
+    private StrategyParameterInt Lot;
+
+    /// <summary>
+    /// риск на сделку в процентах
+    /// </summary>
+    private StrategyParameterDecimal RiskInPercents;
+
+
+    #endregion end Fields ==============================
+
+
+}
+
