648f0e1ebce45bfc884ac48fb409d3348aca2d6e
vv <vv@VV-DOM>
2023-05-29 14:26:45 +0300

    создал вьюшку и вью модель для главного она WPF и положил на него кнопу для вызова команды

--- project/OsEngine/OsaExtension/MVVM/View/MainWindowRobotsWPF.xaml.cs
+++ project/OsEngine/OsaExtension/MVVM/View/MainWindowRobotsWPF.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OsEngine.OsaExtension.MVVM.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,11 @@ namespace OsEngine.OsaExtension.MVVM.View
         public MainWindowRobotsWPF()
         {
             InitializeComponent();
+
+           vm = new MainWindowRobWpfVM();
+           DataContext = vm;
+
         }
+        MainWindowRobWpfVM vm;
     }
 }
---
+++ project/OsEngine/OsaExtension/MVVM/ViewModels/MainWindowRobWpfVM.cs
@@ -0,0 +1,48 @@
+﻿using OsEngine.OsaExtension.MVVM.Commands;
+using OsEngine.OsaExtension.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsEngine.OsaExtension.MVVM.ViewModels
+{
+    /// <summary>
+    /// VM для главного окна с роботами WPF
+    /// </summary>
+    public class MainWindowRobWpfVM : BaseVM
+    {
+        public MainWindowRobWpfVM() 
+        {
+            
+        }
+
+        #region Commands ===========================================================
+
+        private DelegateCommand commandGetListBot;
+        public DelegateCommand CommandGetListBot
+        {
+            get 
+            {
+                if (commandGetListBot == null)
+
+                {
+                    commandGetListBot = new DelegateCommand(GetListBot);
+                }
+                return commandGetListBot;
+            }
+        }
+
+        #endregion
+
+        #region Metods ============================================================
+
+        void GetListBot(object o)
+        {
+
+        }
+
+        #endregion
+    }
+}
