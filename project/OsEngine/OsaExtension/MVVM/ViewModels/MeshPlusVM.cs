b3ba41458d8d8cc958dccc388de437a6107d59e2
vv <vv@VV-DOM>
2023-06-21 11:36:06 +0300

    создал файлы для Сеточника плюс

---
+++ project/OsEngine/OsaExtension/MVVM/ViewModels/MeshPlusVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OsEngine.Entity;
+using OsEngine.Market;
+using OsEngine.Market.Servers;
+using OsEngine.OsaExtension.MVVM.Commands;
+using OsEngine.OsaExtension.MVVM.Models;
+using OsEngine.OsaExtension.MVVM.View;
+using OsEngine.OsTrader;
+using OsEngine.OsTrader.Panels;
+using OsEngine.Robots.Trend;
+using OsEngine.OsTrader.Panels.Tab;
+
+using System.Collections.ObjectModel;
+
+using System.Threading;
+
+
+namespace OsEngine.OsaExtension.MVVM.ViewModels
+{
+    public class MeshPlusVM
+    {
+
+    }
+}
---
+++ project/OsEngine/OsaExtension/Robots/MeshPlus/MeshPlusBot.cs
@@ -0,0 +1,42 @@
+﻿using OsEngine.Alerts;
+using OsEngine.Charts.CandleChart.Elements;
+using OsEngine.Charts.CandleChart.Indicators;
+using OsEngine.Entity;
+using OsEngine.Indicators;
+using OsEngine.Logging;
+using OsEngine.OsaExtension.MVVM.ViewModels;
+using OsEngine.OsaExtension.MyBots.meshOnSMA;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Attributes;
+using OsEngine.OsTrader.Panels.Tab;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace OsEngine.OsaExtension.Robots.MeshPlus
+{
+
+    [Bot("MeshPlusBot")]
+    public class MeshPlusBot : BotPanel
+    {
+        public MeshPlusBot(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+
+        }
+
+        public override string GetNameStrategyType()
+        {
+            return nameof(MeshPlusBot);
+        }
+
+        public override void ShowIndividualSettingsDialog()
+        {
+           
+        }
+    }
+}
