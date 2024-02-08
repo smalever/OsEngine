00a2eb4a2fe5eb9dd52b15801f24d108da620277
vv <vv@VV-DOM>
2024-02-08 13:48:29 +0300

    1

--- project/OsEngine/OsaExtension/MVVM/ViewModels/RobotBreakVM.cs
+++ project/OsEngine/OsaExtension/MVVM/ViewModels/RobotBreakVM.cs
@@ -2082,7 +2082,7 @@ namespace OsEngine.OsaExtension.MVVM.ViewModels
             if (workLot < minVolume)
             {
                 SendStrStatus("Объем ордера меньше допустимого");
-                _logger.Error(" Order volume < minVolume {Method} {minVolume} {workLot}"
+                _logger.Error(" Order volume < minVolume {Method} Minimum {minVolume} {workLot}"
                     , nameof(CalculateVolumeTradesOpen), minVolume, workLot);
                 // IsRun = false;
             }
