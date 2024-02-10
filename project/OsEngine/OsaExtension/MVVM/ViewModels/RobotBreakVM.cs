55f17d8d7255ab589c7a1a0a395fcc2e51b267de
vv <vv@VV-DOM>
2024-02-10 20:39:18 +0300

    1

--- project/OsEngine/OsaExtension/MVVM/ViewModels/RobotBreakVM.cs
+++ project/OsEngine/OsaExtension/MVVM/ViewModels/RobotBreakVM.cs
@@ -32,6 +32,8 @@ using Position = OsEngine.Entity.Position;
 //using Direction = OsEngine.Entity.Position;
 using Security = OsEngine.Entity.Security;
 
+
+
 namespace OsEngine.OsaExtension.MVVM.ViewModels
 {
     public class RobotBreakVM : BaseVM, IRobotVM, IDisposable
